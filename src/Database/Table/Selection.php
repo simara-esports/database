2e031a578949dc502dfa3149b0ab3a64e9ff2a02
Patrik Kolbl <insekticid@gmail.com>
2014-09-17 14:15:55 +0200

    Table: added Selection::fetchAssoc() (interface BC break!) #29

--- src/Database/Table/Selection.php
+++ src/Database/Table/Selection.php
@@ -248,7 +248,7 @@ class Selection extends Nette\Object implements \Iterator, IRowContainer, \Array
 	 */
 	public function fetchPairs($key = NULL, $value = NULL)
 	{
-		return Nette\Database\Helpers::toPairs(iterator_to_array($this), $key, $value);
+		return Nette\Database\Helpers::toPairs($this->fetchAll(), $key, $value);
 	}
 
 
@@ -258,6 +258,15 @@ class Selection extends Nette\Object implements \Iterator, IRowContainer, \Array
 	public function fetchAll()
 	{
 		return iterator_to_array($this);
+	}
+
+
+	/**
+	 * @inheritDoc
+	 */
+	public function fetchAssoc($path)
+	{
+		return Nette\Utils\Arrays::associate($this->fetchAll(), $path);
 	}
 
 
